dfcc2342aa4d618263f9a5ef5188a7c4bc2f7fc1
Govind <govind12771277@gmail.com>
2026-01-29 14:36:58 +0530

    authmiddleware added

---
+++ src/middleware/authmiddleware.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { prisma } from "../../db";
+
+interface AuthRequest extends Request {
+  user?: {
+    id: string;
+    email: string;
+    role: string;
+  };
+}
+
+const authMiddleware = async (req: AuthRequest,res: Response,  next: NextFunction) => {
+  try {
+    // const authHeader = req.headers.authorization;
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        data: null,
+        error: "TOKEN_REQUIRED",
+      });
+    }
+
+
+    const decodedToken = jwt.verify(token as string,process.env.JWT_SECRET as string) as JwtPayload;
+
+    const user = await prisma.users.findUnique({
+      where: { id: decodedToken.id },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        data: null,
+        error: "INVALID_TOKEN",
+      });
+    }
+    
+    console.log(user)
+
+    const {password: _, ...userWithoutPassword} = user;
+
+    req.user = userWithoutPassword;
+    console.log(req.user)
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      data: null,
+      error: "INVALID_OR_EXPIRED_TOKEN",
+    });
+  }
+};
+
+export default authMiddleware;
